8a21ac7e3b1de54690769db47b4e1567ab40c27c
hugo9442 <hugo9442@gmail.com>
2025-03-27 09:30:02 +0000

    added corrections teacher propoused

--- src/js/TodoListApiService.js
+++ src/js/TodoListApiService.js
@@ -1,6 +1,5 @@
 const Url = "https://playground.4geeks.com/todo"
 
-
 export const ApiTodoList = {
     createUser: async (user) => {
         try {
@@ -26,19 +25,19 @@ export const ApiTodoList = {
         } catch (error) {
             console.log(error)
             return error
-
         }
     },
     creattask: async (user, div) => {
-        let task={"is_done": false,"label":`${div}`
-            };
+        let task = {
+            "is_done": false, "label": `${div}`
+        };
         try {
             const request = await fetch(`${Url}/todos/${user}`, {
                 method: "POST",
-                headers:{
-                    "content-Type":"application/json"
+                headers: {
+                    "content-Type": "application/json"
                 },
-                body:JSON.stringify(task)
+                body: JSON.stringify(task)
             })
             const response = await request.json();
             return response
@@ -48,11 +47,11 @@ export const ApiTodoList = {
 
         }
     },
-    deleteTask: async (id,) => {
+    deleteTask: async (id) => {
         try {
             const request = await fetch(`${Url}/todos/${id}`, {
                 method: "DELETE",
-               
+
             })
             const response = await request.json();
             console.log(response)
@@ -64,18 +63,19 @@ export const ApiTodoList = {
         }
     },
     updateTask: async (id, label) => {
-        let task={"is_done": true,"label":`${label}`
-    };
-try {
-    const request = await fetch(`${Url}/todos/${id}`, {
-        method: "PUT",
-        headers:{
-            "content-Type":"application/json"
-        },
-        body:JSON.stringify(task)
-    })
-    const response = await request.json();
-    return response
+        let task = {
+            "is_done": true, "label": `${label}`
+        };
+        try {
+            const request = await fetch(`${Url}/todos/${id}`, {
+                method: "PUT",
+                headers: {
+                    "content-Type": "application/json"
+                },
+                body: JSON.stringify(task)
+            })
+            const response = await request.json();
+            return response
         } catch (error) {
             console.log(error)
             return error
--- src/js/components/GrandList.jsx
+++ src/js/components/GrandList.jsx
@@ -25,7 +25,7 @@ const GrandList = () => {
       console.log(error)
     }
   }
-  const hsndleUser = async () => {
+  const handleUser = async () => {
     try {
       const data = await ApiTodoList.createUser(user)
       if (data.detail === "User already exists.") {
@@ -53,14 +53,17 @@ const GrandList = () => {
 
   const pressEnterUser = (e) => {
     if (e.keyCode === 13) {
-      if (user !== "") { 
-        hsndleUser() }
+      if (user !== "") {
+        handleUser()
+      }
     }
   }
 
   const pressEnter = (e) => {
     if (e.keyCode === 13) {
-      addTarea()
+      if (div !== "") {
+        addTarea()
+      }
     }
   }
   const getTarea = (e) => {
@@ -71,89 +74,91 @@ const GrandList = () => {
     setUser(e.target.value)
   }
 
-  const addTarea = () => {
+  const addTarea = async () => {
     if (user !== "") {
-      handleCreateTask();
-      setTimeout(() => {
+      await
+        handleCreateTask();
+      await
         handleChecK();
-      }, 500);
-
     }
     setDiv("")
   }
 
-  const changeColor = (id, label) => {
+  const changeColor = async (id, label) => {
     const handleUpdateTask = async () => {
       try {
         const data = await ApiTodoList.updateTask(id, label)
-      } catch (error) {
-      }
-    }
-    handleUpdateTask();
-    setTimeout(() => {
+      } catch (error) {}
+      
+     }
+    await
+      handleUpdateTask();
+    await
       handleChecK();
-    }, 500);
-
   }
 
-  const deleteTarea = (id) => {
-    const handleDeleteTask = async () => {
-      try {
-        const data = await ApiTodoList.deleteTask(id)
-      } catch (error) {
-        console.log(error)
+
+
+  const deleteTarea = async (id) => {
+    {
+      const handleDeleteTask = async () => {
+        try {
+          const data = await ApiTodoList.deleteTask(id)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      await
+        handleDeleteTask();
+      await
+        handleChecK();
+      if (value.length == 1) {
+        swal({
+          title: "EMPTY TASK",
+          text: "PLEASE ENTER A TASK",
+          icon: "error",
+          button: "ACEPT",
+          timer: 4000
+        });
       }
     }
-    handleDeleteTask();
-    setTimeout(() => {
-      handleChecK();
-    }, 200);
-
-    if (value.length === 0) {
-      swal({
-        title: "EMPTY TASK",
-        text: "PLEASE ENTER A TASK",
-        icon: "error",
-        button: "ACEPT",
-        timer: 4000
-      });
-    }
   }
-
+  let color = "red"
   let counterValue = value.length
-  let color="red"
+
+
 
   return (
     <div className="p-3 mb-2 bg-body-secondary"> <h1 id="title"> BIG TASK </h1>
-    <div className="user">
-    <h4>USER NAME</h4>
-      <li className="list-group-item bg-body-tertiary">
-        <input type="text" className="form-control bg-body-tertiary"
-          placeholder="USER" value={user} onChange={getuser} onKeyDown={pressEnterUser} />
-      </li>
-    </div>
+      <div className="user">
+        <h4>USER NAME</h4>
+        <li className="list-group-item bg-body-tertiary">
+          <input type="text" className="form-control bg-body-tertiary"
+            placeholder="USER" value={user} onChange={getuser} onKeyDown={pressEnterUser} />
+        </li>
+      </div>
       <ul className="list-group bg-body-tertiary" style={{ filter: "drop-shadow(0px 8px 7px #000000)" }}>
         <li className="list-group-item bg-body-tertiary">
           <input type="text" className="form-control bg-body-tertiary"
             placeholder="What Needs To Be Done" value={div} onChange={getTarea} onKeyDown={pressEnter} />
         </li>
-        {value.map((task) =>{
-          if(task.is_done !== false){
-            color="green"
-          }else{
-            color="red"
+        {value.map((task) => {
+          if (task.is_done !== false) {
+            color = "green"
+          } else {
+            color = "red"
           }
-           return(
-             <li className="list-group-item bg-body-tertiary" key={task.id}>
-            <div className="liDiv">
-              <h5 className="text" style={{ color: `${color}` }}>{task.label}</h5>
-              <div className="twobtn">
-                <button type="button" className="btn btn-success" onClick={() => changeColor(task.id, task.label)}>DONE</button>
-                <button type="button" className="btn-close" labelvalue="Close" onClick={() => deleteTarea(task.id)}></button>
+          return (
+            <li className="list-group-item bg-body-tertiary" key={task.id}>
+              <div className="liDiv">
+                <h5 className="text" style={{ color: `${color}` }}>{task.label}</h5>
+                <div className="twobtn">
+                  <button type="button" className="btn btn-success" onClick={() => changeColor(task.id, task.label)}>DONE</button>
+                  <button type="button" className="btn-close" labelvalue="Close" onClick={() => deleteTarea(task.id)}></button>
+                </div>
               </div>
-            </div>
-          </li>
-           )
+            </li>
+          )
         }
         )}
         <li id="text" className="list-group-item bg-body-tertiary"><p className="text">{counterValue} Item left</p></li>
